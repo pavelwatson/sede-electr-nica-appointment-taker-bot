960ca738db5fff97e6b76c6c7e3672b376d73ac8
Pavel Watson <watsonpavel@gmail.com>
2021-08-03 23:50:59 +0300

    Make code cleaner and more stable. Global code refactoing. Add province and operation selection

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
 import time
-import os
-import random
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, ElementNotInteractableException, UnexpectedAlertPresentException, NoSuchElementException
@@ -13,193 +10,152 @@ import telegram_send
 
 path = "C:/Program Files (x86)/chromedriver.exe"
 PAGE_URL = "https://sede.administracionespublicas.gob.es"
-driver = webdriver.Chrome(path)
+
+
+def return_operation(key):
+    """returns an operation string for selecting it in main cycle"""
+    return {
+        'refugee_documents': 'ASILO-OFICINA DE ASILO Y REFUGIO."nueva normalidad” Expedición/Renovación Documentos.C/ Pradillo 40',
+        'fingerprinting':    'POLICIA-TOMA DE HUELLAS (EXPEDICIÓN DE TARJETA) Y RENOVACIÓN DE TARJETA DE LARGA DURACIÓN'
+    }[key]
+
 
 class Customer:
     doc_value = 'Y1234567M'
     name = 'Forename Surname'
     year = '01/01/2020'
+    province = 'Madrid',
+    operation = return_operation('refugee_documents')
 
 
-#################       MAIN FUNCTIONS         ###########################################
-def action(elem, keys, next_elem=''):
-    """LOOKING FOR ELEMENT, PERFORMING IT AND CHECK IF IT WAS SUCCESFULL"""
-    if wait_for_element(elem) or press_button(elem, keys, next_elem):
-        return True
-    # time.sleep(random.uniform(0.1, 2))
+def return_web_element(key):
+    """returns an element id for web page interaction"""
+    return {
+        'tasks':       (By.XPATH, '//*[@id="mainWindow"]/div[2]/section[1]/div/ul/li[7]/a'),
+        'appoinments': (By.XPATH, '//*[@id="mainWindow"]/div[2]/div/section/div[2]/ul/li/ul/li[1]/div[1]/p/a'),
+        'submit':      (By.ID, 'submit'),
+        'provinces':   (By.ID, 'form'),
+        'aceptar':     (By.ID, 'btnAceptar'),
+        'operation':   (By.ID, 'tramiteGrupo[0]'),
+        'entrar':      (By.ID, 'btnEntrar'),
+        'form':        (By.ID, 'txtIdCitado'),
+        'enviar':      (By.ID, 'btnEnviar'),
+        'consultar':   (By.ID, 'btnConsultar'),
+        'salir':       (By.ID, 'btnSalir'),
+        'appointment': (By.ID, 'btnSiguiente'),
+        'country':     (By.ID, 'txtPaisNac'),
+        'captcha':     (By.ID, 'btnSubmit'),
+        'login':       (By.ID, 'Frm_Username'),
+        'router_tab':  (By.ID, 'mmManager'),
+        'manage_tab':  (By.ID, 'smSysMgr'),
+        'reboot':      (By.ID, 'Submit1')
+    }[key]
 
 
-def wait_for_element(elem):
-    """WAITING FOR ELEMENT TO APPEAR ON A PAGE"""
-    try:
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located(elem)
-            )
-    except TimeoutException:
-        if fail():
-            return True
-    except UnexpectedAlertPresentException:
-        if fail():
-            return True
+def wait_elem(elem, delay=30):
+    """waits for elem element to appear on a page"""
+    WebDriverWait(driver, delay).until(
+        EC.presence_of_element_located(elem)
+        )
 
 
-def press_button(elem, keys, next_elem):
-    """TRYING TO PRESS BUTTON 50 TIMES"""
+def perform_elem(elem, keys):
+    """performs keys on element"""
     for i in range(50):
         try:
-            action = driver.find_element(*elem)
-            action.send_keys(keys)
-            if check_the_page(next_elem):
-                return
+            element = driver.find_element(*elem)
+            element.send_keys(keys)
+            break
         except ElementNotInteractableException:
             continue
-        except NoSuchElementException:
-            captcha = True
-            fail(captcha)
-            return True
-        except UnexpectedAlertPresentException:
-            if fail():
-                return True
-    if fail():
-        return True
 
 
-def check_the_page(next_elem):
-    """CHECKING IF PAGE HAS BEEN PASSED"""
-    if next_elem:
-        try:
-            WebDriverWait(driver, 2).until(
-                        EC.presence_of_element_located((By.ID, next_elem))
-                        )
-            return True
-        except TimeoutException:
-            return False
+def action(elem, keys=Keys.ENTER, next_elem=''):
+    """
+    waits for elem element to appear on a page
+    performs keys on element
+    Check if it was succefull, by waiting for elem to appear on a next page
+    """
+    wait_elem(elem)
+    perform_elem(elem, keys)
+
+
+def prepare_page():
+    """opens PAGE_URL and then setups the page for cycling """
+    driver.get(PAGE_URL)
+    action(return_web_element('tasks'), Keys.ENTER)
+    action(return_web_element('appoinments'), Keys.ENTER)
+    action(return_web_element('submit'), Keys.ENTER)
+
+
+def captcha_validation_check():
+    try:
+        wait_elem(return_web_element('captcha'), 2)
+    except TimeoutException:
+        return
     return True
 
 
-def fail(captcha=''):
-    """RESTARTING THE CYCLE, AFTER FUNCTION FAILS"""
-    if captcha:
-        # restart()
-        print('CAPTCHA VALIDATION HAS FAILED')
-        for i in range(10):
-            os.system('wsay "CAPTCHA VALIDATION HAS FAILED"')
-
-    else:
-        print("Failed to load")
-        for i in range(5):
-            os.system('wsay "FAIL"')
-
-    open_page = True
+def appointment_check():
+    """CHECKING AN APPOINTMENT"""
+    wait_elem(return_web_element('appointment'), 2)
     return True
 
 
+def fill_form(Customer):
+    if Customer.operation == return_operation('refugee_documents'):
+        action(return_web_element('form'), (Customer.doc_value, Keys.TAB, Customer.name, Keys.TAB, Customer.year))
+    elif Customer.operation == return_operation('fingerprinting'):
+        action(return_web_element('form'), (Customer.doc_value, Keys.TAB, Customer.name))
+        action(return_web_element('country'), 'UCRANIA')
+    action(return_web_element('enviar'))
 
-def restart():
-    ### restsart router ###
+
+def reboot_router():
+    ### reboot router ###
     pass
 
 
-def appointment_check(message):
-    """CHECKING APPOINTMENT"""
-    try:
-        WebDriverWait(driver, 2).until(
-            EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".mf-msg__info"), message)
-            )
-    except TimeoutException:
-        print("APPOINTMENT FOUND")
-        while True:
-            os.system('wsay "RETARD ALERT"')
-            telegram_send.send(messages=["Appointment found"])
+def cycle(Customer, path):
+    global driver
+    driver = webdriver.Chrome(path)
 
-    action(
-        (By.ID, 'btnSalir'),
-        Keys.ENTER,
-        'form'
-    )
-    time.sleep(random.uniform(10, 20))
+    atmp_numb = 0
+    while True:
+        try:
+            prepare_page()
 
-atmp_numb = 0
-#CYCLING THE PROCESS
-cycle = True
-open_page = True
-while cycle:
-    while open_page:
-        
-        #OPEN PAGE
-        driver.get(PAGE_URL)
-        action((By.XPATH, '//*[@id="mainWindow"]/div[2]/section[1]/div/ul/li[7]/a'), Keys.ENTER)
-        action((By.XPATH, '//*[@id="mainWindow"]/div[2]/div/section/div[2]/ul/li/ul/li[1]/div[1]/p/a'), Keys.ENTER)
-        action((By.ID, 'submit'), Keys.ENTER)
-        open_page = False
+            # choosing a province
+            action(return_web_element('provinces'), Customer.province)
+            action(return_web_element('aceptar'))
 
-    if action(
-        (By.ID, 'form'),
-        'Madrid'
-    ):
-        open_page = True
-        continue
+            # choosing an operation
+            action(return_web_element('operation'), Customer.operation)
+            action(return_web_element('aceptar'))
+            action(return_web_element('entrar'))
 
-    wait_for_element((By.ID, 'form'))
-    if action(
-        (By.ID, 'btnAceptar'),
-        Keys.ENTER,
-        'tramiteGrupo[0]'
-    ):
-        open_page = True
-        continue
+            # filling a form
+            fill_form(Customer)
 
-    if action(
-        (By.ID, 'tramiteGrupo[0]'),
-        'ASILO-OFICINA DE ASILO Y REFUGIO."nueva normalidad” Expedición/Renovación Documentos.C/ Pradillo 40'
-    ):
-        open_page = True
-        continue
+            # checking captcha validation
+            if captcha_validation_check():
+                reboot_router()
+                print('CAPTCHA VALIDATION ERROR, REBOOTING THE ROUTER')
+                time.sleep(240)
+                continue
+            time.sleep(100)
+            # trying to take an appointment
+            action(return_web_element('enviar'))
 
-    wait_for_element((By.ID, 'tramiteGrupo[0]'))
-    if action(
-        (By.ID, 'btnAceptar'),
-        Keys.ENTER,
-        'btnEntrar'
-    ):
-        open_page = True
-        continue
+            # checking if appointment is available
+            if appointment_check():
+                while True:  # if so, spams the user about it.
+                    telegram_send.send(messages=["Appointment found"])
 
-    if action(
-        (By.ID, 'btnEntrar'),
-        Keys.ENTER,
-        'txtIdCitado'
-    ):
-        open_page = True
-        continue
-    wait_for_element((By.ID, 'txtIdCitado'))
-    if action(
-        (By.ID, 'txtIdCitado'),
-        (Customer.doc_value, Keys.TAB, Customer.name, Keys.TAB, Customer.year),
-    ):
-        open_page = True
-        continue
+        except TimeoutException:
+            atmp_numb += 1
+            print(f'ATTEMPT: {str(atmp_numb)}. NO LUCK... TRYING AGAIN')
+            continue
 
-    if action(
-        (By.ID, 'btnEnviar'),
-        Keys.ENTER,
-        'btnConsultar'
-    ):
-        open_page = True
-        continue
 
-    if wait_for_element((By.ID, 'btnConsultar')):
-        open_page = True
-        continue
-    if action(
-        (By.ID, 'btnEnviar'),
-        Keys.ENTER,
-        'btnSalir'
-    ):
-        open_page = True
-        continue
-
-    appointment_check('En este momento no hay citas disponibles')
-
-    atmp_numb += 1
-    print(f'ATTEMPT: {str(atmp_numb)}. NO LUCK... TRYING AGAIN')
+cycle(Customer, path)
